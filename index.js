51501281521bf9d7818b3fba82b25761202aa6d8
Shruthi Sridhar <shruthisridhar96@gmail.com>
2022-01-09 11:47:19 +0530

    Movies Node JS V6

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import {MongoClient} from 'mongodb';
 import dotenv from 'dotenv';
 import {moviesRouter} from './routes/movies.js';
 dotenv.config();
+
 const mongo_URL=process.env.mongo_URL;
 const Port=process.env.PORT;
 const app=express();
 app.use(express.json());
+
 async function createConnection()
 {
   const client=new MongoClient(mongo_URL);
@@ -14,7 +16,9 @@ async function createConnection()
   console.log("MongoDB connected");
   return client;
 }
+
  export const client=await createConnection();
+ 
 app.get('/',(request,response)=>{
    response.send("Hello World! My name is Shruthi");
 });
